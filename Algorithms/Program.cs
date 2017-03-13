86dae7f935e6625353908d176af4a7103138e055
cperez <cperez@madmobile.com>
2017-03-13 15:11:43 -0400

    Recursive Folder

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,12 +1,12 @@
-﻿using System.Diagnostics;
+﻿using System;
 namespace Algorithms
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            var results = IsPrime(10);
-            Debug.WriteLine($"Is Prime  Value: {results}");
+            CopyFolder(@"c:\code", @"G:\MadMobile\code", "node_modules,bin,.git");
+            Console.ReadLine();
 
         }
     }
