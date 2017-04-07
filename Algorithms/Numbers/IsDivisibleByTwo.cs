d4d6c200012e1f4047407010294d1bb484713d48
cperez <cperez@madmobile.com>
2017-04-07 16:30:07 -0400

    Added PasswordCheck

---
+++ Algorithms/Numbers/IsDivisibleByTwo.cs
@@ -0,0 +1,16 @@
+﻿/* **************************************************************************
+ * Any whole number that ends in 0, 2, 4, 6, or 8 will be divisible by 2.
+ * Here the divisibility test is done by performing the mod function with 2.
+ *****************************************************************************/
+namespace Algorithms
+{
+    partial class Program
+    {
+        public static bool IsDivisbleByTwo(int val)
+        {
+            
+            return val % 2 == 0;
+
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿
+using System;
+
 namespace Algorithms
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            CopyFolder(@"c:\code", @"G:\MadMobile\code", "node_modules,bin,.git");
+            //CopyFolder(@"c:\code", @"G:\MadMobile\code", "node_modules,bin,.git");
+            var test = GetRemainder(5, 3);
             Console.ReadLine();
 
         }
