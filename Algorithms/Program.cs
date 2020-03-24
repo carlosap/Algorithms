46877b9721d442abf4c822bd057e63ec15dcf999
carlosap <perezca@yahoo.com>
2020-03-24 02:14:10 -0400

    Renamed programs

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,13 +1,18 @@
 ﻿
-using System;
+
 
 namespace Algorithms
 {
+    using System;
     partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(PermutationCheck("god", "dog"));
+            var nums = new[] {2, 7, 11, 15};
+            var target = 9;
+            var retVal = TwoSum(nums, target);
+            Console.WriteLine($"Two Sum value {string.Join(",",retVal)}");
+            
             Console.ReadLine();
 
         }
